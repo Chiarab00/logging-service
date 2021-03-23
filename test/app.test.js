1d92b43fe6508087f9fb567fbb097f4746d9903f
unknown <chiara.barzagli.studio@fitstic-edu.com>
2021-03-23 12:02:24 +0100

    commit2

--- test/app.test.js
+++ test/app.test.js
@@ -3,11 +3,13 @@ const app = require('../src/app')
 
 describe('test app rest endpoints', () => {
 
-    test('It should return Hello World', async () => {
+
+    test('It should return Hello World', async() => {
         const response = await request(app).get('/')
 
         expect(response.statusCode).toBe(200)
-        expect(response.body).toEqual('Hello 🍌')
+        expect(response.body).toEqual('Hello World!!!')
+
     })
 
 })
